08dc2342c44dd11be89517486c9dd931051a25d2
TheTheophan <markot688@gmail.com>
2025-09-01 20:14:58 +0200

    completed faktura seeder

--- LaravelApp/zavrsni_rad/database/seeders/FakturaSeeder.php
+++ LaravelApp/zavrsni_rad/database/seeders/FakturaSeeder.php
@@ -12,8 +12,29 @@ class FakturaSeeder extends Seeder
      */
     public function run(): void
     {
-        Faktura::factory()
-            ->count(5)
-            ->create();
+        $tipPaketaPrices = [
+            1 => ['mesecna' => 3500, 'godisnja' => 37000],
+            2 => ['mesecna' => 2500, 'godisnja' => 26500],
+            3 => ['mesecna' => 6000, 'godisnja' => 65000],
+            4 => ['mesecna' => 1800, 'godisnja' => 19000],
+            5 => ['mesecna' => 1200, 'godisnja' => 12500],
+            6 => ['mesecna' => 9000, 'godisnja' => 98000],
+        ];
+
+        $paketi = \App\Models\PaketKorisnika::all();
+        $count = 0;
+        foreach ($paketi as $paket) {
+            if ($count >= 40) break;
+            $tipId = $paket->tip_paketa_id;
+            $godisnja = $paket->godisnja_pretplata;
+            $cena = $godisnja ? $tipPaketaPrices[$tipId]['godisnja'] : $tipPaketaPrices[$tipId]['mesecna'];
+            Faktura::create([
+                'paket_korisnika_id' => $paket->id,
+                'cena' => $cena,
+                'tekst' => '',
+                'placeno' => $count < 20 ? true : false,
+            ]);
+            $count++;
+        }
     }
 }
